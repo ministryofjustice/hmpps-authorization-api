606a4e3f28e525759de5e43ef2f0768bf91f9b82
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2022-06-07 15:16:00 +0100

    HAAR-497: removed RegisteredUser plus associated repository and service as not required for client credentials flow

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauthorizationserver/model/RegisteredUserRepository.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsauthorizationserver.model
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface RegisteredUserRepository : CrudRepository<RegisteredUser, Long> {
-  fun findByUserName(userName: String) : RegisteredUser?
-}
