86546c44b7bb62e9ebd6ebe608745885b7fb9069
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2022-06-07 17:04:11 +0100

    HAAR-497: dev test data now inserted via flyway

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauthorizationserver/config/AuthorizationServerConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauthorizationserver/config/AuthorizationServerConfig.kt
@@ -13,21 +13,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.oauth2.core.AuthorizationGrantType
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod
-import org.springframework.security.oauth2.core.oidc.OidcScopes
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings
-import org.springframework.security.oauth2.server.authorization.config.TokenSettings
 import org.springframework.security.web.SecurityFilterChain
 import uk.gov.justice.digital.hmpps.hmppsauthorizationserver.service.KeyGeneratorUtils
-import java.time.Duration
-import java.util.*
 
 @Configuration(proxyBeanMethods = false)
 class AuthorizationServerConfig {
@@ -70,36 +63,8 @@ class AuthorizationServerConfig {
   ): RegisteredClientRepository {
     val registeredClientRepository = JdbcRegisteredClientRepository(jdbcTemplate)
     val registeredClientParametersMapper = JdbcRegisteredClientRepository.RegisteredClientParametersMapper()
-    val yourClientId = "test-client-id"
-    val yourSecret = BCryptPasswordEncoder(10).encode("test-secret")
-
     registeredClientParametersMapper.setPasswordEncoder(passwordEncoder)
     registeredClientRepository.setRegisteredClientParametersMapper(registeredClientParametersMapper)
-
-    if (registeredClientRepository.findByClientId(yourClientId) == null) {
-      val registeredClient: RegisteredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-        .tokenSettings(
-          TokenSettings.builder()
-            .accessTokenTimeToLive(Duration.ofMinutes(5))
-            .refreshTokenTimeToLive(Duration.ofMinutes(10))
-            .build()
-        )
-        .clientId(yourClientId)
-        .clientSecret(yourSecret)
-        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-        .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-        .redirectUri("http://127.0.0.1:8089/authorized")
-        .redirectUri("https://oauth.pstmn.io/v1/callback")
-        .scope(OidcScopes.OPENID)
-        .scope("test-application.read")
-        .scope("test-application.write")
-        .build()
-
-      registeredClientRepository.save(registeredClient)
-    }
-
     return registeredClientRepository
   }
 
