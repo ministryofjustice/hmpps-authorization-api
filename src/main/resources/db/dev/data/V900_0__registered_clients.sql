5d900068cf49cd58a7924c2b26c897ef5370f109
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2023-06-13 10:14:18 +0100

    HAAR-1447: added client to test data with exclusive client.create scope - required for oidc registration end point usage

--- src/main/resources/db/dev/data/V900_0__registered_clients.sql
+++ src/main/resources/db/dev/data/V900_0__registered_clients.sql
@@ -3,7 +3,12 @@ INSERT INTO oauth2_registered_client
      CLIENT_AUTHENTICATION_METHODS, AUTHORIZATION_GRANT_TYPES, REDIRECT_URIS, SCOPES, CLIENT_SETTINGS, TOKEN_SETTINGS)
 VALUES
     ('ceab6c18-081b-44e9-8130-3b37a254108f', 'test-client-id', current_timestamp, '{bcrypt}$2a$10$iItP8qu7ocHyw92687SKAehZQb7MhCjU6g37OGUt1I0guEE7B.4ba', null, 'alan_t_testing',
-     'client_secret_basic', 'refresh_token,client_credentials,authorization_code', 'http://127.0.0.1:8089/authorized,https://oauth.pstmn.io/v1/callback', 'read,write',
+     'client_secret_basic', 'refresh_token,client_credentials,authorization_code', 'http://127.0.0.1:8089/authorized,https://oauth.pstmn.io/v1/callback', 'read,write,client.create',
+     '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}',
+     '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":true,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",300.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",600.000000000]}'),
+
+    ('83c3869b-696d-4a6e-8da4-3d36666bab9d', 'test-client-create-id', current_timestamp, '{bcrypt}$2a$10$iItP8qu7ocHyw92687SKAehZQb7MhCjU6g37OGUt1I0guEE7B.4ba', null, 'alan_t_testing',
+     'client_secret_basic', 'client_credentials', 'http://127.0.0.1:8089/authorized,https://oauth.pstmn.io/v1/callback', 'client.create',
      '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}',
      '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":true,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",300.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",600.000000000]}'),
 
