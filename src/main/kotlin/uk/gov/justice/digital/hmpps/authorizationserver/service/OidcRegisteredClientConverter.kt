a79a36e2d7e463813a92e6583de40ae72ac0b753
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2023-06-26 18:30:20 +0100

    HAAR-1546: OIDC registration end point now registers using client supplied client id

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/service/OidcRegisteredClientConverter.kt
@@ -0,0 +1,13 @@
+package uk.gov.justice.digital.hmpps.authorizationserver.service
+
+import org.springframework.core.convert.converter.Converter
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+import org.springframework.security.oauth2.server.authorization.oidc.OidcClientRegistration
+
+class OidcRegisteredClientConverter(private val delegateConverter: Converter<OidcClientRegistration, RegisteredClient>) : Converter<OidcClientRegistration, RegisteredClient> {
+
+  override fun convert(clientRegistration: OidcClientRegistration): RegisteredClient? {
+    val registeredClient = delegateConverter.convert(clientRegistration)
+    return RegisteredClient.from(registeredClient).clientId(clientRegistration.clientId).build()
+  }
+}
