8cda01cc54ea049b230d15a7fa72a79a111fc6d9
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2023-07-17 11:11:23 +0100

    HAAR-1565: consolidate flyway scripts

--- src/main/resources/db/auth/V1_0__create_tables.sql
+++ src/main/resources/db/auth/V1_0__create_tables.sql
@@ -25,6 +25,14 @@ CREATE TABLE oauth2_authorization
     refresh_token_issued_at       timestamp     DEFAULT NULL,
     refresh_token_expires_at      timestamp     DEFAULT NULL,
     refresh_token_metadata        text          DEFAULT NULL,
+    user_code_value               text          DEFAULT NULL,
+    user_code_issued_at           timestamp     DEFAULT NULL,
+    user_code_expires_at          timestamp     DEFAULT NULL,
+    oauth2_authorization          text          DEFAULT NULL,
+    device_code_value             text          DEFAULT NULL,
+    device_code_issued_at         timestamp     DEFAULT NULL,
+    device_code_expires_at        timestamp     DEFAULT NULL,
+    device_code_metadata          text          DEFAULT NULL,
     PRIMARY KEY (id)
 );
 
@@ -50,7 +58,33 @@ CREATE TABLE oauth2_registered_client
     scopes                        varchar(1000)                           NOT NULL,
     client_settings               varchar(2000)                           NOT NULL,
     token_settings                varchar(2000)                           NOT NULL,
+    post_logout_redirect_uris     varchar(1000) DEFAULT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE oauth2_client_config
+(
+    base_client_id                varchar(100)                            NOT NULL,
+    allowed_ips                   varchar(1000) DEFAULT NULL,
+    client_end_date               date          DEFAULT NULL,
+    PRIMARY KEY (base_client_id)
+);
+
+CREATE TABLE oauth2_client_deployment_details
+(
+    base_client_id                varchar(100)                            NOT NULL,
+    client_type                   varchar(255)  DEFAULT NULL,
+    team                          varchar(255)  DEFAULT NULL,
+    team_contact                  varchar(255)  DEFAULT NULL,
+    team_slack                    varchar(255)  DEFAULT NULL,
+    hosting                       varchar(255)  DEFAULT NULL,
+    namespace                     varchar(255)  DEFAULT NULL,
+    deployment                    varchar(255)  DEFAULT NULL,
+    secret_name                   varchar(255)  DEFAULT NULL,
+    client_id_key                 varchar(255)  DEFAULT NULL,
+    secret_key                    varchar(255)  DEFAULT NULL,
+    deployment_info               varchar(1000) DEFAULT NULL,
+    PRIMARY KEY (base_client_id)
 );
 
 -- NOTE, the users and authorities tables below can be restructured as necessary
--- src/main/resources/db/auth/V1_3__user_code_device_code.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE oauth2_authorization ADD COLUMN user_code_value text DEFAULT NULL;
-ALTER TABLE oauth2_authorization ADD COLUMN user_code_issued_at timestamp DEFAULT NULL;
-ALTER TABLE oauth2_authorization ADD COLUMN user_code_expires_at timestamp DEFAULT NULL;
-ALTER TABLE oauth2_authorization ADD COLUMN user_code_metadata text DEFAULT NULL;
-
-ALTER TABLE oauth2_authorization ADD COLUMN device_code_value text DEFAULT NULL;
-ALTER TABLE oauth2_authorization ADD COLUMN device_code_issued_at timestamp DEFAULT NULL;
-ALTER TABLE oauth2_authorization ADD COLUMN device_code_expires_at timestamp DEFAULT NULL;
-ALTER TABLE oauth2_authorization ADD COLUMN device_code_metadata text DEFAULT NULL;
