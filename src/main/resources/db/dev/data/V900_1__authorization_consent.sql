b04ebe949da644355ad46efe4773c34d37095c83
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2023-07-21 11:36:49 +0100

    HAAR-1644: minor dev data flyway script tidy up

--- src/main/resources/db/dev/data/V900_1__authorization_consent.sql
+++ src/main/resources/db/dev/data/V900_1__authorization_consent.sql
@@ -1,2 +1,2 @@
 INSERT INTO oauth2_authorization_consent(registered_client_id, principal_name, authorities) VALUES
- ('ceab6c18-081b-44e9-8130-3b37a254108f', 'alan_t_testing', 'ROLE_AUDIT,ROLE_TESTING');
+ ('ceab6c18-081b-44e9-8130-3b37a254108f', 'test-client-1', 'ROLE_OAUTH_ADMIN,ROLE_AUDIT,ROLE_TESTING');
