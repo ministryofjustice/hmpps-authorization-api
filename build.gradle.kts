fae8edbd0e0ffcb3c9f9c25a47513901cbfdf209
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-10-03 10:08:08 +0100

    HAAR-1892: ⬆️ update to spring-boot-plugin-5.5.0 (#82)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.4.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.5.0"
   kotlin("plugin.spring") version "1.9.10"
   kotlin("plugin.jpa") version "1.9.10"
 }
@@ -14,15 +14,15 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.springframework.security:spring-security-oauth2-authorization-server:1.1.2")
 
-  implementation("org.flywaydb:flyway-core:9.22.0")
+  implementation("org.flywaydb:flyway-core:9.22.2")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.hibernate:hibernate-core:6.3.0.Final")
+  implementation("org.hibernate:hibernate-core:6.3.1.Final")
 
   implementation("commons-codec:commons-codec")
   implementation("org.apache.commons:commons-text:1.10.0")
-  implementation("io.opentelemetry:opentelemetry-api:1.30.0")
+  implementation("io.opentelemetry:opentelemetry-api:1.30.1")
 
-  runtimeOnly("com.h2database:h2:2.2.222")
+  runtimeOnly("com.h2database:h2:2.2.224")
   runtimeOnly("org.postgresql:postgresql:42.6.0")
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
