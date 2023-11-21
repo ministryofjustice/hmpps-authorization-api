1f46f6eedce44fd40120a860da30cb5c0fa66215
ravmoj <104509282+ravmoj@users.noreply.github.com>
2023-11-21 09:45:10 +0000

    HAAR-2057: Add public key endpoint (jwt-public-key) (#96)
    * HAAR-2057: Add new  Public JWT Key (formatted and base 64 encoded version)'jwt-public-key' endpoint.
    
    * HAAR-2057: Add new  Public JWT Key (formatted and base 64 encoded version)'jwt-public-key' endpoint.
    
    * HAAR-2057: Add new  Public JWT Key (formatted and base 64 encoded version)'jwt-public-key' endpoint.
    
    * HAAR-2057: Added test PublicKeyIntTest.kt

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/resource/PublicKeyController.kt
@@ -0,0 +1,52 @@
+package uk.gov.justice.digital.hmpps.authorizationserver.resource
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.apache.commons.codec.binary.Base64
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.authorizationserver.service.JWKKeyAccessor
+import java.security.PublicKey
+
+@Tag(name = "jwt-public-key")
+@RestController
+class PublicKeyController @Autowired constructor(
+  jwkKeyAccessor: JWKKeyAccessor,
+) {
+  private val publicKey: PublicKey
+
+  @RequestMapping(value = ["jwt-public-key"], produces = [MediaType.APPLICATION_JSON_VALUE])
+  @Operation(summary = "Public JWT Key", description = " ")
+  fun getJwtPublicKey(): Map<String, Any> {
+    val formattedKey = getFormattedKey(publicKey)
+    return mapOf(
+      "formatted" to convertNewLinesToArray(formattedKey),
+      "encoded" to Base64.encodeBase64String(formattedKey.toByteArray()),
+    )
+  }
+
+  private fun convertNewLinesToArray(formattedKey: String): Array<String> =
+    formattedKey.split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+
+  private fun getFormattedKey(pk: PublicKey): String {
+    val builder = StringBuilder()
+    val encodeBase64String = Base64.encodeBase64String(pk.encoded)
+    builder.append("-----BEGIN PUBLIC KEY-----")
+    builder.append("\n")
+    var i = 0
+    while (i < encodeBase64String.length) {
+      builder.append(encodeBase64String, i, Math.min(i + 64, encodeBase64String.length))
+      builder.append("\n")
+      i += 64
+    }
+    builder.append("-----END PUBLIC KEY-----")
+    builder.append("\n")
+    return builder.toString()
+  }
+
+  init {
+    publicKey = jwkKeyAccessor.getPrimaryKeyPair().public
+  }
+}
