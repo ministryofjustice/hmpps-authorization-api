6829a024888b8fdd48e6d723df2d711ae4b95d7e
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2023-12-04 11:28:15 +0000

    HAAR-1826: reinstate register authorization code client test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/integration/OidcIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/integration/OidcIntTest.kt
@@ -1,8 +1,16 @@
 package uk.gov.justice.digital.hmpps.authorizationserver.integration
 
+import org.assertj.core.api.Assertions.assertThat
+import org.json.JSONObject
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
+import org.springframework.web.reactive.function.BodyInserters
 import uk.gov.justice.digital.hmpps.authorizationserver.data.repository.ClientRepository
+import uk.gov.justice.digital.hmpps.authorizationserver.service.RegisteredClientAdditionalInformation
+import java.util.Base64
 
 class OidcIntTest : IntegrationTestBase() {
 
@@ -12,47 +20,47 @@ class OidcIntTest : IntegrationTestBase() {
   @Nested
   inner class Registration {
 
-    // @Test
-    // fun `should register client using given client id`() {
-    //   val clientCredentialsResponse = webTestClient
-    //     .post().uri("/oauth2/token?grant_type=client_credentials")
-    //     .header(
-    //       HttpHeaders.AUTHORIZATION,
-    //       "Basic " + Base64.getEncoder().encodeToString(("test-client-create-id:test-secret").toByteArray()),
-    //     )
-    //     .exchange()
-    //     .expectStatus().isOk
-    //     .expectBody()
-    //     .returnResult().responseBody
-    //
-    //   val accessToken = JSONObject(String(clientCredentialsResponse)).get("access_token")
-    //
-    //   webTestClient
-    //     .post().uri("/connect/register")
-    //     .header(HttpHeaders.AUTHORIZATION, "Bearer $accessToken")
-    //     .body(
-    //       BodyInserters.fromValue(
-    //         mapOf(
-    //           "client_id" to "authorization_code_register_test",
-    //           "client_name" to "authorization_code_registration_test",
-    //           "access_token_validity" to "1200",
-    //           "scope" to "read write",
-    //           "authorities" to "MAINTAIN_OAUTH_USERS,AUTH_GROUP_MANAGER",
-    //           "token_endpoint_auth_method" to "client_secret_basic",
-    //           "jwks_uri" to "https://client.example.org/my_public_keys.jwks",
-    //           "grant_types" to "authorization_code",
-    //           "redirect_uris" to "http://localhost:3000",
-    //           "jira_number" to "HAAR-1999",
-    //           "response_types" to "id_token",
-    //         ),
-    //       ),
-    //     )
-    //     .exchange()
-    //     .expectStatus().isCreated
-    //
-    //   val registeredClient = clientRepository.findClientByClientId("authorization_code_register_test")
-    //   assertNotNull(registeredClient)
-    //   assertThat(registeredClient!!.tokenSettings.settings[RegisteredClientAdditionalInformation.JIRA_NUMBER_KEY]).isEqualTo("HAAR-1999")
-    // }
+    @Test
+    fun `should register client using given client id`() {
+      val clientCredentialsResponse = webTestClient
+        .post().uri("/oauth2/token?grant_type=client_credentials")
+        .header(
+          HttpHeaders.AUTHORIZATION,
+          "Basic " + Base64.getEncoder().encodeToString(("test-client-create-id:test-secret").toByteArray()),
+        )
+        .exchange()
+        .expectStatus().isOk
+        .expectBody()
+        .returnResult().responseBody
+
+      val accessToken = JSONObject(String(clientCredentialsResponse)).get("access_token")
+
+      webTestClient
+        .post().uri("/connect/register")
+        .header(HttpHeaders.AUTHORIZATION, "Bearer $accessToken")
+        .body(
+          BodyInserters.fromValue(
+            mapOf(
+              "client_id" to "authorization_code_register_test",
+              "client_name" to "authorization_code_registration_test",
+              "access_token_validity" to "1200",
+              "scope" to "read write",
+              "authorities" to "MAINTAIN_OAUTH_USERS,AUTH_GROUP_MANAGER",
+              "token_endpoint_auth_method" to "client_secret_basic",
+              "jwks_uri" to "https://client.example.org/my_public_keys.jwks",
+              "grant_types" to "authorization_code",
+              "redirect_uris" to "http://localhost:3000",
+              "jira_number" to "HAAR-1999",
+              "response_types" to "id_token",
+            ),
+          ),
+        )
+        .exchange()
+        .expectStatus().isCreated
+
+      val registeredClient = clientRepository.findClientByClientId("authorization_code_register_test")
+      assertNotNull(registeredClient)
+      assertThat(registeredClient!!.tokenSettings.settings[RegisteredClientAdditionalInformation.JIRA_NUMBER_KEY]).isEqualTo("HAAR-1999")
+    }
   }
 }
