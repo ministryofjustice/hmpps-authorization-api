25a024ea109fd012b22fa42d6135dd155240ae21
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-12-15 12:13:49 +0000

    HAAR-2154: ⬆️ update to java gradle spring boot plugin 5.10.1 (#100)
    * HAAR-2154: ⬆️ update to java gradle spring boot plugin 5.10.1
    
    * HAAR-2154: ⬆️ update to java gradle spring boot plugin 5.10.1
    
    * HAAR-2154: revert spring-security-oauth2-authorization-server dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.8.0"
-  kotlin("plugin.spring") version "1.9.20"
-  kotlin("plugin.jpa") version "1.9.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.10.1"
+  kotlin("plugin.spring") version "1.9.21"
+  kotlin("plugin.jpa") version "1.9.21"
 }
 
 configurations {
@@ -14,17 +14,17 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.springframework.security:spring-security-oauth2-authorization-server:1.1.3")
 
-  implementation("org.flywaydb:flyway-core:9.22.3")
+  implementation("org.flywaydb:flyway-core")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-  implementation("org.hibernate:hibernate-core:6.3.1.Final")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+  implementation("org.hibernate:hibernate-core:6.4.0.Final")
 
   implementation("commons-codec:commons-codec")
-  implementation("org.apache.commons:commons-text:1.10.0")
+  implementation("org.apache.commons:commons-text:1.11.0")
   implementation("io.opentelemetry:opentelemetry-api")
 
   runtimeOnly("com.h2database:h2:2.2.224")
-  runtimeOnly("org.postgresql:postgresql:42.6.0")
+  runtimeOnly("org.postgresql:postgresql:42.7.1")
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
   testImplementation("org.springframework.boot:spring-boot-starter-webflux")
