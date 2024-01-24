568cebb2148e029168e08ebf0db7b51f7c1050f9
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2024-01-24 14:05:15 +0000

    HAAR-2281: ⬆️ update to spring-boot-plugin-5.15.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.14.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.1"
   kotlin("plugin.spring") version "1.9.22"
   kotlin("plugin.jpa") version "1.9.22"
 }
@@ -12,12 +12,12 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("org.springframework.security:spring-security-oauth2-authorization-server:1.1.3")
+  implementation("org.springframework.security:spring-security-oauth2-authorization-server:1.2.1")
 
   implementation("org.flywaydb:flyway-core")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
-  implementation("org.hibernate:hibernate-core:6.4.1.Final")
+  implementation("org.hibernate:hibernate-core:6.4.2.Final")
 
   implementation("commons-codec:commons-codec")
   implementation("org.apache.commons:commons-text:1.11.0")
