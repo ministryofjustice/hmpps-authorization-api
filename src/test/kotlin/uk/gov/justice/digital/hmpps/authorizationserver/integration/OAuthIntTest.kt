b571730039847c096353e1984f8fe80db5cf030a
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2024-02-08 18:41:51 +0000

    HAAR-2190: wip - back up commit

--- src/test/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/integration/OAuthIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/integration/OAuthIntTest.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.authorizationserver.integration
 
 import com.microsoft.applicationinsights.TelemetryClient
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import org.assertj.core.api.Assertions.assertThat
 import org.json.JSONArray
 import org.json.JSONObject
@@ -8,11 +10,19 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.verify
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.HttpHeaders
+import uk.gov.justice.digital.hmpps.authorizationserver.service.AuthSource
+import uk.gov.justice.digital.hmpps.authorizationserver.service.JWKKeyAccessor
+import java.time.Duration
 import java.util.Base64
+import java.util.Date
 
 class OAuthIntTest : IntegrationTestBase() {
+
+  @Autowired
+  private lateinit var jwkKeyAccessor: JWKKeyAccessor
 
   @MockBean
   private lateinit var telemetryClient: TelemetryClient
@@ -175,6 +185,82 @@ class OAuthIntTest : IntegrationTestBase() {
         .post().uri("/oauth2/token?grant_type=client_credentials")
         .exchange()
         .expectStatus().isUnauthorized
+    }
+  }
+
+  @Nested
+  inner class AuthorizationCode {
+
+    @Test
+    fun `unauthenticated user`() {
+      webTestClient
+        .get().uri("/oauth2/authorize?response_type=code&client_id=test-auth-code-client&state=1234&redirect_uri=http://127.0.0.1:8089/login/oauth2/code/oidc-client")
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
+
+    @Test
+    fun `invalid request - client id`() {
+    }
+
+    @Test
+    fun `invalid request - redirect url`() {
+    }
+
+    @Test
+    fun `invalid request - response type`() {
+    }
+
+    @Test
+    fun `invalid request - state`() {
+    }
+
+    @Test
+    fun `success redirects with code`() {
+      webTestClient
+        .get().uri("/oauth2/authorize?response_type=code&client_id=test-auth-code-client&state=1234&redirect_uri=http://127.0.0.1:8089/login/oauth2/code/oidc-client")
+        .cookie("jwtSession", createAuthenticationJwt())
+        .exchange()
+        .expectStatus().isFound
+        .expectHeader()
+        .exists("Location")
+
+      /*
+      ACTUAL RESPONSE EXAMPLE
+
+      < 302 FOUND Found
+      < X-Content-Type-Options: [nosniff]
+      < X-XSS-Protection: [0]
+      < Cache-Control: [no-cache, no-store, max-age=0, must-revalidate]
+      < Pragma: [no-cache]
+      < Expires: [0]
+      < X-Frame-Options: [DENY]
+      < Location: [http://127.0.0.1:8089/login/oauth2/code/oidc-client?code=jSnFrrkyAHR537emb1ecK8mo00mKhszKuBGaRi8z11QGbhI7t1-9hEVLYKuTGL4HwWbpjct3vD-Vx_-_v4XjDQKFRGFfRY6k7uB3ZXKww7W2cgMiTk5qdM1hSevJJiXI&state=1234]
+      < Content-Length: [0]
+      < Date: [Thu, 08 Feb 2024 18:35:39 GMT]
+
+       */
+    }
+
+    private fun createAuthenticationJwt(): String {
+      val username = "username"
+      val authoritiesAsString = "ROLE_TESTING,ROLE_MORE_TESTING"
+
+      return Jwts.builder()
+        .setId("1234")
+        .setSubject(username)
+        .addClaims(
+          mapOf<String, Any>(
+            "authorities" to authoritiesAsString,
+            "name" to "name",
+            "auth_source" to AuthSource.Auth.name,
+            "user_id" to "9999",
+            "passed_mfa" to true,
+          ),
+        )
+        .setExpiration(Date(System.currentTimeMillis() + Duration.ofMinutes(5).toMillis()))
+        .signWith(SignatureAlgorithm.RS256, jwkKeyAccessor.getPrimaryKeyPair().private)
+        .compact()
     }
   }
 
