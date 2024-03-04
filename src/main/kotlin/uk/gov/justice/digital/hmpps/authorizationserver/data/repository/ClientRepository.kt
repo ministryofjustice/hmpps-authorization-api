4390cff96565e59f9cfea5c10a5eff4da2271f07
R Kumar <ravindra.kumar@digital.justice.gov.uk>
2024-03-04 13:59:23 +0000

    HAAR-2422: Added Query annotation to delete row. Deleting Client entity , was trying to remove records from 'oauth2_authorization'.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/data/repository/ClientRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/data/repository/ClientRepository.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.authorizationserver.data.repository
 
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import uk.gov.justice.digital.hmpps.authorizationserver.data.model.Client
 
@@ -8,5 +10,7 @@ interface ClientRepository : CrudRepository<Client, String> {
 
   fun findByClientIdStartsWithOrderByClientId(clientId: String): List<Client>
 
+  @Modifying
+  @Query("delete from Client c where c.clientId=:clientId")
   fun deleteByClientId(clientId: String)
 }
