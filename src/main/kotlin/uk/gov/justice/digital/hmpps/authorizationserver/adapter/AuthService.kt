7eda8aef18c63c968890e658e8cf9b4dd35e54ed
R Kumar <ravindra.kumar@digital.justice.gov.uk>
2024-03-20 13:49:27 +0000

    HAAR-2378: removed 'authEnabled' feature shield

--- src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/adapter/AuthService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/adapter/AuthService.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.authorizationserver.adapter
 
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
