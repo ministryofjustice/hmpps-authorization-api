8bbdcd38aafc95d524b38c9c49367a7c73c9c6c9
R Kumar <ravindra.kumar@digital.justice.gov.uk>
2024-03-21 22:22:03 +0000

    HAAR-2464: New endpoint to expose fields for auth ClientDetails interface

--- src/test/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/integration/MigrationControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/authorizationserver/integration/MigrationControllerIntTest.kt
@@ -443,6 +443,54 @@ class MigrationControllerIntTest : IntegrationTestBase() {
     }
   }
 
+  @Nested
+  inner class ListAllClientDetails {
+    @Test
+    fun `access unauthorized when no authority`() {
+      webTestClient.get().uri("/client-details")
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
+
+    @Test
+    fun `access forbidden when no role`() {
+      webTestClient.get().uri("/client-details")
+        .headers(setAuthorisation(roles = listOf()))
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `access forbidden when wrong role`() {
+      webTestClient.get().uri("/client-details")
+        .headers(setAuthorisation(roles = listOf("WRONG")))
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `list client details success`() {
+      val matchByClientId = "$[?(@.clientId == '%s')]"
+      webTestClient.get().uri("/client-details")
+        .headers(setAuthorisation(roles = listOf("ROLE_OAUTH_ADMIN")))
+        .exchange()
+        .expectStatus().isOk
+        .expectHeader().contentType(MediaType.APPLICATION_JSON)
+        .expectBody()
+        .jsonPath("$.[*]").value<List<String>> { assertThat(it).hasSize(10) }
+        .jsonPath(matchByClientId, "test-client-id").exists()
+        .jsonPath(matchByClientId, "test-client-create-id").exists()
+        .jsonPath(matchByClientId, "ip-allow-a-client-1").exists()
+        .jsonPath(matchByClientId, "ip-allow-b-client").exists()
+        .jsonPath(matchByClientId, "ip-allow-b-client-8").exists()
+        .jsonPath(matchByClientId, "test-duplicate-id").exists()
+        .jsonPath(matchByClientId, "test-complete-details-id").exists()
+        .jsonPath(matchByClientId, "ip-allow-c-client").exists()
+        .jsonPath(matchByClientId, "test-auth-code-client").exists()
+        .jsonPath(matchByClientId, "hmpps-auth-authorization-server-client").exists()
+    }
+  }
+
   private fun verifyAuthorities(id: String, clientId: String, vararg authorities: String): AuthorizationConsent {
     val authorizationConsent = authorizationConsentRepository.findById(AuthorizationConsent.AuthorizationConsentId(id, clientId)).get()
     assertThat(authorizationConsent.authorities).containsOnly(*authorities)
