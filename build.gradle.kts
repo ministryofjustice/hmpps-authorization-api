a4f8287a0e901ab8889bcc441d0d773289cf41e4
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2024-03-28 12:03:09 +0000

    HAAR-2467: ⬆️ update to spring-boot-plugin-5.15.4 (#128)
    * HAAR-2467: ⬆️ update to spring-boot-plugin-5.15.4
    
    * HAAR-2467: reverted spring authorization server to 1.1.6
    
    * HAAR-2467: reverted spring authorization server to 1.1.3
    
    * HAAR-2467: updated spring authorization server to 1.2.3
    
    * HAAR-2467: remove hibernate core as already included with spring jpa

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.1"
-  kotlin("plugin.spring") version "1.9.22"
-  kotlin("plugin.jpa") version "1.9.22"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.4"
+  kotlin("plugin.spring") version "1.9.23"
+  kotlin("plugin.jpa") version "1.9.23"
 }
 
 configurations {
@@ -12,14 +12,13 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("org.springframework.security:spring-security-oauth2-authorization-server:1.1.3")
+  implementation("org.springframework.security:spring-security-oauth2-authorization-server:1.2.3")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.flywaydb:flyway-core")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
-  implementation("org.hibernate:hibernate-core:6.4.2.Final")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
 
   implementation("commons-codec:commons-codec")
   implementation("org.apache.commons:commons-text:1.11.0")
@@ -27,16 +26,16 @@ dependencies {
 
   implementation("io.jsonwebtoken:jjwt:0.12.5")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
-  implementation("com.sun.xml.bind:jaxb-impl:4.0.3")
-  implementation("com.sun.xml.bind:jaxb-core:4.0.3")
+  implementation("com.sun.xml.bind:jaxb-impl:4.0.5")
+  implementation("com.sun.xml.bind:jaxb-core:4.0.5")
 
   runtimeOnly("com.h2database:h2:2.2.224")
-  runtimeOnly("org.postgresql:postgresql:42.7.1")
+  runtimeOnly("org.postgresql:postgresql:42.7.3")
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
   testImplementation("org.springframework.boot:spring-boot-starter-webflux")
-  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.4")
-  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.4")
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
   implementation(kotlin("stdlib"))
 }
 
