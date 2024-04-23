9657b6e2469c6d9c7666c0011a76b6020737fc7a
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2024-04-23 14:33:22 +0100

    HAAR-2557: Removed obsolete tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/integration/MigrationControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/integration/MigrationControllerIntTest.kt
@@ -10,8 +10,6 @@ import org.junit.jupiter.api.Test
 import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.http.MediaType
-import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository
 import org.springframework.web.reactive.function.BodyInserters
 import uk.gov.justice.digital.hmpps.authorizationapi.data.model.AuthorizationConsent
 import uk.gov.justice.digital.hmpps.authorizationapi.data.model.ClientType
@@ -21,7 +19,6 @@ import uk.gov.justice.digital.hmpps.authorizationapi.data.repository.Authorizati
 import uk.gov.justice.digital.hmpps.authorizationapi.data.repository.ClientConfigRepository
 import uk.gov.justice.digital.hmpps.authorizationapi.data.repository.ClientDeploymentRepository
 import uk.gov.justice.digital.hmpps.authorizationapi.data.repository.ClientRepository
-import uk.gov.justice.digital.hmpps.authorizationapi.utils.OAuthClientSecret
 import java.time.Duration
 import java.time.LocalDate
 
@@ -40,13 +37,7 @@ class MigrationControllerIntTest : IntegrationTestBase() {
   lateinit var clientDeploymentRepository: ClientDeploymentRepository
 
   @MockBean
-  lateinit var oAuthClientSecretGenerator: OAuthClientSecret
-
-  @MockBean
   private lateinit var telemetryClient: TelemetryClient
-
-  @Autowired
-  lateinit var jdbcRegisteredClientRepository: JdbcRegisteredClientRepository
 
   @Nested
   inner class MigrateClient {
@@ -385,70 +376,6 @@ class MigrationControllerIntTest : IntegrationTestBase() {
       )
 
       clientRepository.delete(client)
-    }
-  }
-
-  @Nested
-  inner class ListAllClientDetails {
-    @Test
-    fun `access unauthorized when no authority`() {
-      webTestClient.get().uri("/client-details")
-        .exchange()
-        .expectStatus().isUnauthorized
-    }
-
-    @Test
-    fun `access forbidden when no role`() {
-      webTestClient.get().uri("/client-details")
-        .headers(setAuthorisation(roles = listOf()))
-        .exchange()
-        .expectStatus().isForbidden
-    }
-
-    @Test
-    fun `access forbidden when wrong role`() {
-      webTestClient.get().uri("/client-details")
-        .headers(setAuthorisation(roles = listOf("WRONG")))
-        .exchange()
-        .expectStatus().isForbidden
-    }
-
-    @Test
-    fun `list client details success`() {
-      val matchByClientId = "$[?(@.clientId == '%s')]"
-      webTestClient.get().uri("/client-details")
-        .headers(setAuthorisation(roles = listOf("ROLE_OAUTH_ADMIN")))
-        .exchange()
-        .expectStatus().isOk
-        .expectHeader().contentType(MediaType.APPLICATION_JSON)
-        .expectBody()
-        .jsonPath("$.[*]").value<List<String>> { assertThat(it).hasSize(10) }
-        .jsonPath("\$.[0].clientId").isEqualTo("test-client-id")
-        .jsonPath("\$.[0].accessTokenValiditySeconds").isEqualTo(300)
-        .jsonPath("\$.[0].refreshTokenValiditySeconds").isEqualTo(600)
-        .jsonPath("\$.[0].redirectUris").isEqualTo("http://127.0.0.1:8089/authorized,https://oauth.pstmn.io/v1/callback")
-        .jsonPath("\$.[0].jwtFields").isEmpty
-        .jsonPath("\$.[0].scopes[0]").isEqualTo("read")
-        .jsonPath("\$.[0].scopes[1]").isEqualTo("write")
-        .jsonPath("\$.[0].resourceIds[*]").doesNotExist()
-        .jsonPath("\$.[0].jiraNumber").isEmpty
-        .jsonPath("\$.[0].databaseUserName").isEmpty
-        .jsonPath("\$.[0].skipToAzureField").isBoolean
-        .jsonPath("\$.[0].mfaRememberMe").isBoolean
-        .jsonPath("\$.[0].authorities[*]").value<List<String>> {
-          assertThat(it)
-            .containsExactlyInAnyOrder("ROLE_OAUTH_ADMIN", "ROLE_AUDIT", "ROLE_TESTING", "ROLE_VIEW_AUTH_SERVICE_DETAILS")
-        }
-        .jsonPath(matchByClientId, "test-client-id").exists()
-        .jsonPath(matchByClientId, "test-client-create-id").exists()
-        .jsonPath(matchByClientId, "ip-allow-a-client-1").exists()
-        .jsonPath(matchByClientId, "ip-allow-b-client").exists()
-        .jsonPath(matchByClientId, "ip-allow-b-client-8").exists()
-        .jsonPath(matchByClientId, "test-duplicate-id").exists()
-        .jsonPath(matchByClientId, "test-complete-details-id").exists()
-        .jsonPath(matchByClientId, "ip-allow-c-client").exists()
-        .jsonPath(matchByClientId, "test-auth-code-client").exists()
-        .jsonPath(matchByClientId, "hmpps-auth-authorization-api-client").exists()
     }
   }
 
