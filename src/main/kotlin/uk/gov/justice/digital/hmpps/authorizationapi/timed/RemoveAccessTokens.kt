daf27ee7eaf37ddb902317d8dff9295ca4c962f2
AlTurner-MOJ <101654848+AlTurner-MOJ@users.noreply.github.com>
2024-11-06 11:03:27 +0000

    HAAR-3265: Now using defaultLockAtLeastFor setting, also added info logs (#192)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/timed/RemoveAccessTokens.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/timed/RemoveAccessTokens.kt
@@ -13,48 +13,45 @@ import uk.gov.justice.digital.hmpps.authorizationapi.data.repository.Authorizati
 class RemoveAccessTokens(private val service: RemoveAccessTokensService, private val telemetryClient: TelemetryClient) {
 
   @Scheduled(cron = "\${application.authentication.cron.remove-access-tokens}", zone = "Europe/London")
-  @SchedulerLock(name = "removeAllButLatestAccessToken", lockAtLeastFor = "PT3M")
+  @SchedulerLock(name = "removeAllButLatestAccessToken")
   fun removeAllButLatestAccessToken() {
     try {
-      log.trace("removeAllButLatestAccessToken scheduled task started")
+      log.info("removeAllButLatestAccessToken scheduled task started")
       val numberOfRecordsDeleted = service.removeAllButLatestAccessToken()
 
-      log.trace("removeAllButLatestAccessToken scheduled task removed {} access token records", numberOfRecordsDeleted)
+      log.info("removeAllButLatestAccessToken scheduled task removed {} access token records", numberOfRecordsDeleted)
 
       telemetryClient.trackEvent("AuthorizationApiAllButLatestAccessToken", mapOf("numberOfRecordsDeleted" to numberOfRecordsDeleted.toString()), null)
     } catch (e: Exception) {
-      // have to catch the exception here otherwise scheduling will stop
       log.error("Caught exception {} during removal of all but latest access tokens", e.javaClass.simpleName, e)
     }
   }
 
   @Scheduled(cron = "\${application.authentication.cron.remove-expired-auth-codes}", zone = "Europe/London")
-  @SchedulerLock(name = "removeAllExpiredAuthorizationCodeRecordsWithoutAccessTokens", lockAtLeastFor = "PT3M")
+  @SchedulerLock(name = "removeAllExpiredAuthorizationCodeRecordsWithoutAccessTokens")
   fun removeAllExpiredAuthorizationCodeRecordsWithoutAccessTokens() {
     try {
-      log.trace("removeAllExpiredAuthorizationCodeRecordsWithoutAccessTokens scheduled task started")
+      log.info("removeAllExpiredAuthorizationCodeRecordsWithoutAccessTokens scheduled task started")
       val numberOfRecordsDeleted = service.removeAllExpiredAuthorizationCodeRecordsWithoutAccessTokens()
 
-      log.trace("removeAllExpiredAuthorizationCodeRecordsWithoutAccessTokens scheduled task removed {} expired authorisation code records without access tokens", numberOfRecordsDeleted)
+      log.info("removeAllExpiredAuthorizationCodeRecordsWithoutAccessTokens scheduled task removed {} expired authorisation code records without access tokens", numberOfRecordsDeleted)
       telemetryClient.trackEvent("AuthorizationApiAllExpiredAuthCodeRecordsWithoutAccessTokens", mapOf("numberOfRecordsDeleted" to numberOfRecordsDeleted.toString()), null)
     } catch (e: Exception) {
-      // have to catch the exception here otherwise scheduling will stop
       log.error("Caught exception {} during access token removal", e.javaClass.simpleName, e)
     }
   }
 
   @Scheduled(cron = "\${application.authentication.cron.remove-expired-user-details}", zone = "Europe/London")
-  @SchedulerLock(name = "removeExpiredAuthorizationCodeUsers", lockAtLeastFor = "PT3M")
+  @SchedulerLock(name = "removeExpiredAuthorizationCodeUsers")
   fun removeExpiredAuthorizationCodeUsers() {
     try {
-      log.trace("removeExpiredAuthorizationCodeUsers scheduled task started")
+      log.info("removeExpiredAuthorizationCodeUsers scheduled task started")
       val numberOfRecordsDeleted = service.deleteExpiredAuthorizationCodeUsers()
 
-      log.trace("removeExpiredAuthorizationCodeUsers scheduled task removed {} expired authorisation code user records", numberOfRecordsDeleted)
+      log.info("removeExpiredAuthorizationCodeUsers scheduled task removed {} expired authorisation code user records", numberOfRecordsDeleted)
 
       telemetryClient.trackEvent("AuthorizationApiExpiredAuthCodeUsers", mapOf("numberOfRecordsDeleted" to numberOfRecordsDeleted.toString()), null)
     } catch (e: Exception) {
-      // have to catch the exception here otherwise scheduling will stop
       log.error("Caught exception {} during access token removal from oauth2_user_authorization_code", e.javaClass.simpleName, e)
     }
   }
