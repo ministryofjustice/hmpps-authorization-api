25933c691fef8b9930db0d025b1c219f79b96ff9
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2024-11-26 13:09:36 +0000

    HAAR-3355 fix invalid url encoding character parsing

--- src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/service/UrlDecodingRetryClientSecretAuthenticationProvider.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/service/UrlDecodingRetryClientSecretAuthenticationProvider.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.authorizationapi.service
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException
+import org.springframework.security.oauth2.core.OAuth2Error
+import org.springframework.security.oauth2.core.OAuth2ErrorCodes
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken
 import java.net.URLDecoder
 import java.nio.charset.StandardCharsets
@@ -16,7 +18,7 @@ class UrlDecodingRetryClientSecretAuthenticationProvider(
       delegate.authenticate(authentication)
     } catch (e: OAuth2AuthenticationException) {
       authentication?.credentials?.let {
-        val urlDecodedCredentials = URLDecoder.decode(it.toString(), StandardCharsets.UTF_8.toString())
+        val urlDecodedCredentials = decodeCredentials(it.toString())
 
         val clientAuthentication = authentication as OAuth2ClientAuthenticationToken
         val decodedAuthentication = OAuth2ClientAuthenticationToken(
@@ -29,6 +31,12 @@ class UrlDecodingRetryClientSecretAuthenticationProvider(
         delegate.authenticate(decodedAuthentication)
       }
     }
+  }
+
+  private fun decodeCredentials(credentials: String): String? = try {
+    URLDecoder.decode(credentials, StandardCharsets.UTF_8.toString())
+  } catch (e: Exception) {
+    throw OAuth2AuthenticationException(OAuth2Error(OAuth2ErrorCodes.INVALID_CLIENT), e)
   }
 
   override fun supports(authentication: Class<*>): Boolean {
