cce50be12a88554c1688824e46f0b44c8916bb6e
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2025-01-29 08:47:18 +0000

    HAAR-3531: import dev data from legacy auth (#208)

--- src/main/resources/db/dev/data/V900_2__deployment_details.sql
+++ src/main/resources/db/dev/data/V900_2__deployment_details.sql
@@ -7,3 +7,11 @@ VALUES
     ('test-client-create-id', 'PERSONAL', 'HAAR', 'Testy McTester', '#hmpps-auth-audit-registers', 'CLOUDPLATFORM',
      'hmpps-audit-dev', 'hmpps-audit-dev', 'AUDIT_SECRET', 'AUDIT_API_KEY',
      'AUDIT_SECRET_KEY', null);
+
+-- Dev data from Auth --
+
+INSERT INTO oauth2_client_deployment_details (base_client_id, client_type, team, team_contact, team_slack, hosting, namespace, deployment, secret_name, client_id_key, secret_key, deployment_info)
+VALUES  ('another-delete-test-client', 'SERVICE', 'A Team', 'A Team contact', 'A team slack', 'CLOUDPLATFORM', 'another-delete-test-dev', 'another-delete-test-deployment', 'another-delete-test-secret', 'API_CLIENT_ID', 'API_CLIENT_SECRET', 'More info blah'),
+        ('individual-client', 'PERSONAL', 'Bob', 'Bob@digital.justice.gov.uk', 'bob slack', 'OTHER', null, null, null, null, null, null),
+        ('service-client', 'SERVICE', 'A Team', 'A Team contact', 'A team slack', 'CLOUDPLATFORM', 'service-dev', 'service-deployment', 'service-secret', 'API_CLIENT_ID', 'API_CLIENT_SECRET', 'More info blah'),
+        ('another-test-client', 'SERVICE', 'A Team', 'A Team contact', 'A team slack', 'CLOUDPLATFORM', 'duplicate-dev', 'duplicate-deployment', 'duplicate-secret', 'API_CLIENT_ID', 'API_CLIENT_SECRET', 'More info blah');
---
+++ src/main/resources/db/dev/data/V900_4__client_config.sql
@@ -0,0 +1,16 @@
+INSERT INTO oauth2_client_config (base_client_id, allowed_ips, client_end_date)
+VALUES
+    ('ip-allow-a-client', '127.0.0.1/32', null),
+    ('ip-allow-b-client', '35.176.93.186', null),
+    ('ip-allow-c-client', '35.176.0.0/16', null),
+    ('expiry-test-client', '35.176.0.0/16', '2023-01-01');
+
+-- Dev Data From Auth --
+
+INSERT INTO oauth2_client_config (base_client_id, allowed_ips, client_end_date)
+VALUES  ('end-date-client', '', null),
+        ('expired-end-date-client', '', null),
+        ('hmpps-audit-api-client', 'localhost,group-a', null),
+        ('ip-allow-group-client', '35.176.0.0/16,group_name_a', null),
+        ('service-client', '127.0.0.1', null),
+        ('another-test-client', '127.0.0.1', null);
