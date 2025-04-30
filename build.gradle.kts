e10562793fcc2ea930d745f331b52f759d076a79
AlTurner-MOJ <101654848+AlTurner-MOJ@users.noreply.github.com>
2025-04-30 11:04:44 +0100

    HAAR-3898: ⬆️ update to spring-boot-plugin-8.1.0 (#227)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.1.0"
   kotlin("plugin.spring") version "2.1.20"
   kotlin("plugin.jpa") version "2.1.20"
 }
@@ -21,11 +21,11 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
   implementation("org.json:json:20250107")
   implementation("commons-codec:commons-codec")
-  implementation("org.apache.commons:commons-text:1.13.0")
+  implementation("org.apache.commons:commons-text:1.13.1")
   implementation("io.opentelemetry:opentelemetry-api")
   implementation("io.jsonwebtoken:jjwt:0.12.6")
-  implementation("net.javacrumbs.shedlock:shedlock-spring:6.3.1")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:6.3.1")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:6.4.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:6.4.0")
 
   runtimeOnly("com.h2database:h2:2.3.232")
   runtimeOnly("org.postgresql:postgresql:42.7.5")
