81fd5d49205af6cabf41930f9af80e3107dceaeb
ma226860 <142583965+ma226860@users.noreply.github.com>
2025-05-23 10:57:59 +0100

    Dependency Upgrades (#229)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.1.0"
-  kotlin("plugin.spring") version "2.1.20"
-  kotlin("plugin.jpa") version "2.1.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.2.0"
+  kotlin("plugin.spring") version "2.1.21"
+  kotlin("plugin.jpa") version "2.1.21"
 }
 
 configurations {
@@ -18,14 +18,14 @@ dependencies {
   implementation("org.springframework.security:spring-security-oauth2-authorization-server")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.flywaydb:flyway-core")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
-  implementation("org.json:json:20250107")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+  implementation("org.json:json:20250517")
   implementation("commons-codec:commons-codec")
   implementation("org.apache.commons:commons-text:1.13.1")
   implementation("io.opentelemetry:opentelemetry-api")
   implementation("io.jsonwebtoken:jjwt:0.12.6")
-  implementation("net.javacrumbs.shedlock:shedlock-spring:6.4.0")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:6.4.0")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:6.7.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:6.7.0")
 
   runtimeOnly("com.h2database:h2:2.3.232")
   runtimeOnly("org.postgresql:postgresql:42.7.5")
