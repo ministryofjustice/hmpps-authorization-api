03d5f0a3cea0cddf1edaec81c313b61c889e7bd1
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2025-11-14 12:01:56 +0000

    HAAR-4546: remove obsolete AuthService and associated config, also added Auth endpoint and issuer urls to preprod helm file

--- src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/config/WebClientConfiguration.kt
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
@@ -16,20 +15,6 @@ import org.springframework.web.reactive.function.client.WebClient
 class WebClientConfiguration(
   @Value("\${hmpps-auth.endpoint.url}") private val hmppsAuthUri: String,
 ) {
-
-  @Bean
-  fun authorizedClientManagerAppScope(
-    clientRegistrationRepository: ClientRegistrationRepository,
-    oAuth2AuthorizedClientService: OAuth2AuthorizedClientService,
-  ): OAuth2AuthorizedClientManager {
-    val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
-    val authorizedClientManager = AuthorizedClientServiceOAuth2AuthorizedClientManager(
-      clientRegistrationRepository,
-      oAuth2AuthorizedClientService,
-    )
-    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
-    return authorizedClientManager
-  }
 
   @Bean
   fun authorizedAnonymousClientManagerAppScope(
@@ -46,12 +31,6 @@ class WebClientConfiguration(
   @Bean
   fun authHealthWebClient(
     @Qualifier(value = "authorizedAnonymousClientManagerAppScope") authorizedClientManager: OAuth2AuthorizedClientManager,
-    builder: WebClient.Builder,
-  ): WebClient = getOAuthWebClient(authorizedClientManager, builder, hmppsAuthUri, "hmpps-auth")
-
-  @Bean
-  fun authWebClient(
-    @Qualifier(value = "authorizedClientManagerAppScope") authorizedClientManager: OAuth2AuthorizedClientManager,
     builder: WebClient.Builder,
   ): WebClient = getOAuthWebClient(authorizedClientManager, builder, hmppsAuthUri, "hmpps-auth")
 
