47c528935eeb257459babb4f44147b16980e5b1c
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2025-12-18 11:15:23 +0000

    Revert "HAAR-4755: re-instate client IP address check (#264)"
    This reverts commit 31233adf13f819d8756e931ce98dce9dc389417d.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/service/ClientCredentialsRequestValidator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/service/ClientCredentialsRequestValidator.kt
@@ -8,16 +8,12 @@ import org.springframework.security.core.AuthenticationException
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientCredentialsAuthenticationToken
 import uk.gov.justice.digital.hmpps.authorizationapi.data.repository.ClientConfigRepository
-import uk.gov.justice.digital.hmpps.authorizationapi.security.AuthIpSecurity
-import uk.gov.justice.digital.hmpps.authorizationapi.utils.IpAddressHelper
 import java.time.LocalDate
 
 class ClientCredentialsRequestValidator(
   private val delegate: AuthenticationProvider,
   private val clientConfigRepository: ClientConfigRepository,
-  private val ipAddressHelper: IpAddressHelper,
   private val clientIdService: ClientIdService,
-  private val authIpSecurity: AuthIpSecurity,
 ) : AuthenticationProvider {
 
   companion object {
@@ -31,15 +27,10 @@ class ClientCredentialsRequestValidator(
       val clientId = (clientCredentialsAuthentication.principal as OAuth2ClientAuthenticationToken).registeredClient?.clientId
       val baseClientId = clientIdService.toBase(clientId!!)
       val clientConfig = clientConfigRepository.findByIdOrNull(baseClientId)
-      val clientIpAddress = ipAddressHelper.retrieveIpFromRequest()
 
       if (clientConfig?.clientEndDate != null && clientConfig.clientEndDate!!.isBefore(LocalDate.now())) {
         log.warn("Client id $baseClientId has expired")
         throw ClientExpiredException(clientConfig.baseClientId)
-      }
-
-      if (!clientConfig?.ips.isNullOrEmpty()) {
-        authIpSecurity.validateClientIpAllowed(clientIpAddress, clientConfig.ips, clientId)
       }
 
       return delegate.authenticate(authentication)
@@ -50,7 +41,5 @@ class ClientCredentialsRequestValidator(
 
   override fun supports(authentication: Class<*>): Boolean = OAuth2ClientCredentialsAuthenticationToken::class.java.isAssignableFrom(authentication)
 }
-
-class IPAddressNotAllowedException : AuthenticationException("Unable to issue token as request is not from ip within allowed list")
 
 class ClientExpiredException(clientId: String) : AuthenticationException("Client $clientId has expired")
