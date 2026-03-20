bb9d60f8704238d8e3b601d41d8c522842780b15
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2026-03-20 10:37:48 +0000

    HAAR-5045: Added spring-boot-starter-flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-flyway")
   implementation("org.springframework.security:spring-security-oauth2-authorization-server")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.flywaydb:flyway-core")
