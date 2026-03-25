3530169a7a1810d323770b258e38b854dbdffb11
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2026-03-25 12:35:57 +0000

    remove custom forbidden logic for device endpoints as these are now disabled by default

--- src/test/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/integration/DefaultProtocolEndpointsIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/authorizationapi/integration/DefaultProtocolEndpointsIntTest.kt
@@ -9,28 +9,6 @@ class DefaultProtocolEndpointsIntTest : IntegrationTestBase() {
   private val authorization: String = "Basic ${Base64.getEncoder().encodeToString(("test-client-id:test-secret").toByteArray())}"
 
   @Test
-  fun `calling the default protocol endpoint device authorization is denied`() {
-    webTestClient
-      .post()
-      .uri("/oauth2/device_authorization")
-      .header("Authorization", authorization)
-      .body(BodyInserters.fromFormData("client_id", "test-client-id"))
-      .exchange()
-      .expectStatus().isForbidden
-  }
-
-  @Test
-  fun `calling the default protocol endpoint device verification is denied`() {
-    webTestClient
-      .post()
-      .uri("/oauth2/device_verification")
-      .header("Authorization", authorization)
-      .body(BodyInserters.fromFormData("user_code", "JDTQ-CKTT"))
-      .exchange()
-      .expectStatus().isForbidden
-  }
-
-  @Test
   fun `calling the default protocol endpoint oauth2 introspect is denied`() {
     webTestClient
       .post()
