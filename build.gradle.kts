03899c67a1f5adf4c44fd20fa3ab1dceabc8ac82
simon.mitchell <simon.mitchell1@justice.gov.uk>
2026-04-29 10:52:07 +0100

    HAAR-5250: keep springdoc-openapi-starter-webmvc-ui at 3.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
   implementation("org.springframework.security:spring-security-oauth2-authorization-server")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.flywaydb:flyway-core")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
   constraints {
     implementation("org.webjars:swagger-ui:5.32.2")
   }
